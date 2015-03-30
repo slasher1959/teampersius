554e88bf5279ce49d097aa7d0b561670d8873112
Ray448 <yiklamsit@gmail.com>
2015-03-30 00:10:20 -0700

    Bottom_nav bar example
    Bottom_nav bar added to \lecture\index-for_testing.html

---
+++ page/js/bottom_nav.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+		$("#hide-me").click(function(){
+			$("#heading").hide();	
+		});
+		
+		$("#show-me").click(function(){
+			$("#heading").show();	
+		});
+		
+		$("#toggle-me").click(function(){
+			$("#bottom_nav").toggle();	
+		});
+		
+		$("#slide-up").click(function(){
+			$("#heading").slideUp();	
+		});
+		
+		$("#slide-down").click(function(){
+			$("#heading").slideDown();	
+		});
+		
+		$("#slide-toggle").click(function(){
+		    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+			$("#bottom_nav").slideToggle();	
+		});
+		
+		$("#add-class").click(function(){
+			$("#heading").addClass("addClass");	
+		});
+		
+		$("#remove-class").click(function(){
+			$("#heading").removeClass("addClass");	
+		});
+});
